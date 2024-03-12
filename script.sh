3d9b8db6e6f5ea84f036083525c0d906c797ef84
Ghost <108669382+Wemubis@users.noreply.github.com>
2024-03-12 09:17:47 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,14 +5,10 @@ INVENTORY="inventory"
 
 # Define playbook directories
 PLAYBOOKS_DIR="/root/playbooks"
-LIN_DIR="$PLAYBOOKS_DIR/linux"
-WIN_DIR="$PLAYBOOKS_DIR/windows"
 
 # Lists of playbooks to install and uninstall for Linux and Windows
-I_LINUX=("snmp.yml" "usr_pwd.yml")
-I_WINDOWS=()
-U_LINUX=("del_snmp.yml" "del_usr_pwd.yml")
-U_WINDOWS=()
+I_FILES=("snmp.yml" "usr.yml")
+R_FILES=("del_snmp.yml" "del_usr.yml")
 
 # Function to check the existence of playbooks
 check_playbooks_existence() {
@@ -51,21 +47,17 @@ fi
 
 # Check existence of playbooks based on action to be performed
 if [[ "$1" == "-i" || "$1" == "--install" ]]; then
-    check_playbooks_existence "$LIN_DIR" "${I_LINUX[@]}"
-    check_playbooks_existence "$WIN_DIR" "${I_WINDOWS[@]}"
+    check_playbooks_existence "$PLAYBOOKS_DIR" "${I_FILES[@]}"
 elif [[ "$1" == "-r" || "$1" == "--remove" ]]; then
-    check_playbooks_existence "$LIN_DIR" "${U_LINUX[@]}"
-    check_playbooks_existence "$WIN_DIR" "${U_WINDOWS[@]}"
+    check_playbooks_existence "$PLAYBOOKS_DIR" "${R_FILES[@]}"
 fi
 
 # Execute appropriate playbooks based on provided argument
 case "$1" in
     -i | --install)
-        execute_playbooks "$LIN_DIR" "${I_LINUX[@]}"
-        execute_playbooks "$WIN_DIR" "${I_WINDOWS[@]}"
+        execute_playbooks "$PLAYBOOKS_DIR" "${I_FILES[@]}"
         ;;
     -r | --remove)
-        execute_playbooks "$LIN_DIR" "${U_LINUX[@]}"
-        execute_playbooks "$WIN_DIR" "${U_WINDOWS[@]}"
+        execute_playbooks "$PLAYBOOKS_DIR" "${R_FILES[@]}"
         ;;
 esac
