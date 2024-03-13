387640e58cc698a4d5457d99241db770e5767972
Ghost <108669382+Wemubis@users.noreply.github.com>
2024-03-13 10:56:44 +0100

    Update create_vault.sh

--- create_vault.sh
+++ create_vault.sh
@@ -10,7 +10,7 @@ if [[ "$1" == "-c" ]]; then
     echo "create vault $SECRETS\n"
     ansible-vault create $SECRETS --vault-password-file=$PASSWORD
 elif [[ "$1" == "-e" ]]; then
-    echo "create vault $SECRETS with $PASSWORD\n"
+    echo "encrypt vault $SECRETS with $PASSWORD\n"
     ansible-vault encrypt $SECRETS --vault-password-file=$PASSWORD
 elif [[ "$1" == "-d" ]]; then
     echo "decrypt vault $SECRETS with $PASSWORD\n"
